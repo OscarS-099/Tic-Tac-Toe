8287352bb95c1c98b940dd1a85e300689eaf2822
Oscar Stanford <oestanford@perse.co.uk>
2020-04-20 10:07:50 +0100

    gitignore and board printing

--- Game.py
+++ Game.py
@@ -4,14 +4,21 @@ class Game:
     EMPTY = " "
     DIM = 3
     P1 = "o"
-    P2 = x
+    P2 = "x"
 
     def __init__(self):
         self._board = [[Game.EMPTY for _ in range(Game.DIM)] for _ in range(Game.DIM)]
         self._player = Game.P1
 
     def __repr__(self):
-        return ""
+        string = "  1 2 3\n"
+        string += f"1 {self._board[0][0]}|{self._board[0][1]}|{self._board[0][2]}\n"
+        string += "  -----\n"
+        string += f"2 {self._board[1][0]}|{self._board[1][1]}|{self._board[1][2]}\n"
+        string += "  -----\n"
+        string += f"1 {self._board[2][0]}|{self._board[2][1]}|{self._board[2][2]}\n"
+        string += f"\n{self._player} turn to play"
+        return string
 
     def play(self,row,col):
         row -= 1
