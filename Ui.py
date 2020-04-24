e7a90b2aed4280cf8934e5f478d57fbf44466ee3
Oscar Stanford <oestanford@perse.co.uk>
2020-04-24 15:09:22 +0100

    Stage 3 complete - basic GUI

--- Ui.py
+++ Ui.py
@@ -1,5 +1,8 @@
-from abc import ABC, abstractmethod
+from tkinter import Button, Tk, Toplevel, Frame, N,S,E,W,X,Y, LEFT,RIGHT, END, Scrollbar, Text, Message, Grid, StringVar
 from Game import Game, GameError
+from sys import stderr
+from itertools import product
+from abc import ABC, abstractmethod
 
 class Ui(ABC):
 
@@ -9,10 +12,72 @@ class Ui(ABC):
 
 class Gui(Ui):
     def __init__(self):
+        root = Tk()
+        root.title("Tic Tac Toe")
+        frame = Frame(root)
+        frame.pack(fill=X)
+        Button(
+            frame,
+            text='Show Help',
+            command=self.help_callback
+        ).pack(fill=X)
+
+        Button(
+            frame,
+            text='Play Game',
+            command=self.play_callback
+        ).pack(fill=X)
+
+        Button(
+            frame,
+            text='Quit',
+            command=root.quit
+        ).pack(fill=X)
+
+        self._root = root
+
+    def play_callback(self):
+        self._game = Game()
+        game_win = Toplevel(self._root)
+        game_win.title("Game")
+        frame = Frame(game_win)
+        frame.grid(row=0,column=0)
+
+        self._buttons = [[None for _ in range(Game.DIM)] for _ in range(Game.DIM)]
+
+        for row,col in product(range(Game.DIM),range(Game.DIM)):
+            b = StringVar()
+            b.set(self._game.at(row+1,col+1))
+
+            cmd = lambda r=row,c=col : self.play_and_refresh(r,c)
+
+            Button(
+                frame,
+                textvariable=b,
+                command=cmd
+            ).grid(row=row,column=col)
+
+            self._buttons[row][col] = b
+
+        Button(game_win, text="Dismiss", command=game_win.destroy).grid(row=1,column=0)
+
+    def help_callback(self):
         pass
 
+
+
+    def play_and_refresh(self,row,col):
+        try:
+            self._game.play(row+1,col+1)
+        except GameError as e:
+            print(e)
+
+        text = self._game.at(row+1,col+1)
+        self._buttons[row][col].set(text)
+
     def run(self):
-        pass
+        self._root.mainloop()
+
 
 class Terminal(Ui):
     def __init__(self):
